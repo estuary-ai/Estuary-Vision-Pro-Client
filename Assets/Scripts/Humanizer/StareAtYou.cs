cc3d82acd6447ac98bcf2762ccece4ae1247cd77
lemonlemonde <mirudjun@gmail.com>
2024-09-14 18:04:36 -0700

    :sparkles: Stares and headbops at you while speaking

--- Assets/Scripts/Humanizer/StareAtYou.cs
+++ Assets/Scripts/Humanizer/StareAtYou.cs
@@ -6,21 +6,37 @@ class StareAtYou : MonoBehaviour {
 
     [SerializeField] private Animator agentAnimator;
     private static readonly int IsSpeaking = Animator.StringToHash("isSpeaking");
-    private GameObject head;
+    private GameObject stareTransformer;
+    private GameObject nodTransformer;
     private Transform target;
+    [SerializeField] private float nodSpeed = 9.0f;
+    [SerializeField] private float nodAmplitude = 5.0f;
 
     public void Start() {
-        head = this.gameObject.transform.Find("body_head").gameObject;
-        // target = Camera.main.transform;
-        target = GameObject.Find("debug_box").transform;
+        stareTransformer = GameObject.Find("StareTransformer").gameObject;
+        nodTransformer = GameObject.Find("NodTransformer").gameObject;
+        target = Camera.main.transform;
+        // target = GameObject.Find("debug_box").transform;
     }
 
     public void LateUpdate() {
         if (agentAnimator.GetBool(IsSpeaking) == true) {
-            // // turn head to face the camera
-            // Debug.Log("Turning head to stare at you");
-            // head.transform.LookAt(target);
-            agentAnimator.SetLookAtPosition(target.position);
+            float stepSize = 2.0f * Time.deltaTime;
+            
+            // turn head to face the camera
+            // if not already facing the camera
+            Vector3 targetStareDir = target.position - stareTransformer.transform.position;
+            if (Vector3.Angle(stareTransformer.transform.rotation.eulerAngles, targetStareDir) > 5.0f) {
+                Debug.Log("Not facing camera! Turning head to face the camera");
+                
+                Vector3 newStareDir = Vector3.RotateTowards(stareTransformer.transform.forward, targetStareDir, stepSize, 0.0f);
+                stareTransformer.transform.rotation = Quaternion.LookRotation(newStareDir);
+            }
+            
+
+            float nodAmount = Mathf.Sin(Time.time * nodSpeed) * nodAmplitude;
+            nodTransformer.transform.localRotation = Quaternion.Euler(nodAmount, 0, 0);
         }
     }
+
 }
