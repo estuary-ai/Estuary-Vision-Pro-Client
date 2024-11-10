a68b26c095e6bcd1a2a598608579d776b5232b59
Spencer Lin <linspenc@usc.edu>
2024-11-10 11:17:10 -0800

    CHI Study Settings

--- Assets/Scripts/Humanizer/SpeakingIdle.cs
+++ Assets/Scripts/Humanizer/SpeakingIdle.cs
@@ -14,16 +14,16 @@ class SpeakingIdle : MonoBehaviour {
         // Check if BotVoice is speaking
         if (botVoice.IsSpeaking()) {
             // Only change if not already speaking
-            if (agentAnimator.GetBool(IsSpeaking) == false) {
+            // if (agentAnimator.GetBool(IsSpeaking) == false) {
                 Debug.Log("Starting speaking animation");
                 agentAnimator.SetBool(IsSpeaking, true);
-            }
+            // }
         } else {
             // Only change if not already silent
-            if (agentAnimator.GetBool(IsSpeaking) == true) {
+            // if (agentAnimator.GetBool(IsSpeaking) == true) {
                 Debug.Log("Stopping speaking animation");
                 agentAnimator.SetBool(IsSpeaking, false);
-            }
+            // }
         }
     }
 }
